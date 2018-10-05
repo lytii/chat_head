6f9fe32b935f7096e0b5977fa1f633881b5702c9
Benjamin Wong <benjamin.wong@ip-10-38-7-105.intwlan.hq.locationlabs.com>
2018-10-05 01:24:25 -0700

    UI shell

--- app/src/main/java/location/chat_head/service/ChatHeadService.kt
+++ app/src/main/java/location/chat_head/service/ChatHeadService.kt
@@ -5,21 +5,38 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.IBinder
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowManager
 import location.chat_head.R
 
+
 class ChatHeadService : Service() {
 
    var view: View? = null
    var windowManager: WindowManager? = null
+   var collapsedView: View? = null
+   var expandedView: View? = null
+   var expandedButton: View? = null
+
+   var wrapParams = WindowManager.LayoutParams(
+           WindowManager.LayoutParams.WRAP_CONTENT,
+           WindowManager.LayoutParams.WRAP_CONTENT,
+           WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+           WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+           PixelFormat.TRANSLUCENT
+   )
+   var expandParams = WindowManager.LayoutParams(
+           WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+           WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+           PixelFormat.TRANSLUCENT
+   )
 
    override fun onBind(intent: Intent): IBinder? {
       return null
    }
-
    override fun onCreate() {
       super.onCreate()
       setupHead()
@@ -27,34 +44,41 @@ class ChatHeadService : Service() {
 
    private fun setupHead() {
       view = LayoutInflater.from(this).inflate(R.layout.layout_head, null)
-      val params = WindowManager.LayoutParams(
-         WindowManager.LayoutParams.TYPE_PHONE,
-         WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-         PixelFormat.TRANSLUCENT
-      )
-      params.gravity = Gravity.TOP or Gravity.END
-      params.x = -1
-      params.y = 100
-      windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-      windowManager?.addView(view, params)
 
-      view?.setOnClickListener { expandHead(it) }
+      wrapParams.gravity = Gravity.TOP or Gravity.END
+      wrapParams.x = -1
+      wrapParams.y = 100
+
+      windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+      windowManager?.addView(view, wrapParams)
+      collapsedView = view?.findViewById<View>(R.id.collapsed_view)
+      expandedView = view?.findViewById<View>(R.id.expanded_view)
+      expandedButton = view?.findViewById<View>(R.id.expanded_button)
+      collapsedView?.setOnClickListener { expandHead(view!!) }
+      Log.d("View", "setuphead")
    }
 
    private fun expandHead(view: View) {
-      view.findViewById<View>(R.id.collapsed_view).visibility = View.GONE
-      view.findViewById<View>(R.id.expanded_view).apply {
-         visibility = View.VISIBLE
-         setOnClickListener { collapseHead(view) }
-      }
+      windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+      windowManager?.removeView(view)
+      windowManager?.addView(view, expandParams)
+      collapsedView?.visibility = View.GONE
+      expandedView?.visibility = View.VISIBLE
+      expandedButton?.setOnClickListener { collapseHead(view) }
+      //expandedView?.setOnClickListener { collapseHead(view) }
+      collapsedView?.setOnClickListener { null }
+      Log.d("View", "Expanded")
    }
 
    private fun collapseHead(view: View) {
-      view.findViewById<View>(R.id.expanded_view).visibility = View.GONE
-      view.findViewById<View>(R.id.collapsed_view).apply {
-         visibility = View.VISIBLE
-         setOnClickListener { expandHead(view) }
-      }
+      windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+      windowManager?.removeView(view)
+      windowManager?.addView(view, wrapParams)
+      collapsedView?.visibility = View.VISIBLE
+      expandedView?.visibility = View.GONE
+      collapsedView?.setOnClickListener { expandHead(view) }
+      expandedButton?.setOnClickListener { null }
+      Log.d("View", "Collapse")
    }
 
    override fun onDestroy() {
